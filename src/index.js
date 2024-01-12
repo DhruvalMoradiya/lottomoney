d3366c547e9d0198de4744abbcad631b3c69283a
Appholic6 <133737474+Appholic6@users.noreply.github.com>
2024-01-12 16:18:53 +0530

    withdrow

--- src/index.js
+++ src/index.js
@@ -19,7 +19,7 @@ app.use((req, res, next) => {
   }
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST,PUT, DELETE');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, x-api-key, adminid,');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization,authorization, adminid,');
   res.setHeader('Access-Control-Expose-Headers', 'stickerdeletepass');
   next();
 });
